f2ca7c44529ba019f387e443d5bd5f0e161ca76c
Aquaharmonics <aquaharmonics@gmail.com>
2020-07-03 09:58:52 -0700

    paraview bugfix
    create folder before creating 'bodies.txt' file to avoid "No such file or directory" on fopen() call on windowd
    use correct arity when calling waveElevationGrid()

--- source/objects/waveClass.m
+++ source/objects/waveClass.m
@@ -363,7 +363,7 @@ classdef waveClass<handle
                 filename = [pathParaviewVideo,'\\vtk' filesep 'waves' filesep 'waves_' num2str(it) '.vtp'];
                 fid = fopen(filename, 'w');
                 % calculate wave elevation
-                Z = waveElevationGrid (obj, t(it), X, Y, TimeBodyParav, it, g);
+                Z = waveElevationGrid (obj, t(it), X, Y);
                 % write header
                 fprintf(fid, '<?xml version="1.0"?>\n');
                 fprintf(fid, ['<!-- WEC-Sim Visualization using ParaView -->\n']);
